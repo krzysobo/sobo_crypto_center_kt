4d132c6551b7adb00215a17329d0d97d19c14e76
Krzysztof Sobolewski <krzysztof.sobolewski@gmail.com>
2025-07-02 17:24:34 +0200

    RE #desktop app fix

--- composeApp/src/desktopMain/kotlin/com/krzysobo/cryptocenter/desktopapp.kt
+++ composeApp/src/desktopMain/kotlin/com/krzysobo/cryptocenter/desktopapp.kt
@@ -9,10 +9,11 @@ import com.krzysobo.cryptocenter.appres.AppRes
 import com.krzysobo.cryptocenter.settings.SCC_ROUTE_HANDLE
 import com.krzysobo.cryptocenter.settings.soboCryptoCenterRouteHandlesForDesktopMenu
 import com.krzysobo.cryptocenter.settings.soboCryptoCenterRoutes
+import com.krzysobo.soboapptpl.service.AnyRes
 import com.krzysobo.soboapptpl.service.LocaleManager
 import com.krzysobo.soboapptpl.service.SoboRouter
+import com.krzysobo.soboapptpl.service.anyResText
 import com.krzysobo.soboapptpl.widgets.menus.PageTabsWithOutletAndLogin
-import org.jetbrains.compose.resources.stringResource
 import org.jetbrains.compose.ui.tooling.preview.Preview
 
 @Composable
@@ -29,7 +30,7 @@ fun SoboCryptoCenterDesktopApp() {
 
     SoboTheme {
         Column {
-            Text(stringResource(AppRes.string.app_name_sobo_crypto_center_desktop))
+            Text(anyResText(AnyRes(AppRes.string.app_name_sobo_crypto_center_desktop)))
             PageTabsWithOutletAndLogin(
                 soboCryptoCenterRouteHandlesForDesktopMenu,
                 soboCryptoCenterRoutes
